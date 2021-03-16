19bdc85aec991103a88c982d7ddb4ae062930db4
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2021-03-16 19:09:25 +0000

    reduced tilt speed

--- src/components/card.js
+++ src/components/card.js
@@ -25,7 +25,7 @@ const Card = ({ weather }) => {
                     tiltMaxAngleY={35}
                     perspective={900}
                     scale={1.1}
-                    transitionSpeed={2000}
+                    transitionSpeed={10000}
                     gyroscope={true}>
 
                     <div className="cards">
